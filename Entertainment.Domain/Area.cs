d99ae146ae9270b0e9f12748082bf0db28fc9f03
Vasily <vasily_pavlov_98@mail.ru>
2022-10-03 20:21:01 +0700

    Create domain class and enums

---
+++ Entertainment.Domain/Area.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entertainment.Domain
+{
+    public enum Area
+    {
+        Soviet = 0,
+        Oktyabrsky = 1,
+        Leninsky = 2,
+        Kirovsky = 3
+    }
+}
---
+++ Entertainment.Domain/Entertainment.cs
@@ -0,0 +1,20 @@
+﻿namespace Entertainment.Domain
+{
+    public class Entertainment
+    {
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Not used yet
+        /// </summary>
+        public Guid UserId { get; set; }
+        public long Price { get; set; }
+        public TypeEntertainment TypeEntertainment { get; set; }
+        public string? Details { get; set; }
+        public Area Area { get; set; }
+        /// <summary>
+        /// Maybe not worked with double
+        /// </summary>
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
