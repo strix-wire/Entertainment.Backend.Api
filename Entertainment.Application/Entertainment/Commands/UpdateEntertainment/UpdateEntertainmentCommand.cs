1e87138c43d2ecd62a12bfb113f5f1121e276062
Vasily Pavlov <Pavlov.V@nppstels.ru>
2022-10-05 14:35:46 +0700

    Commands Update implementation

---
+++ Entertainment.Application/Entertainment/Commands/UpdateEntertainment/UpdateEntertainmentCommand.cs
@@ -0,0 +1,27 @@
+﻿using Entertainment.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entertainment.Application.Entertainment.Commands.UpdateEntertainment;
+
+internal class UpdateEntertainmentCommand : IRequest
+{
+    public Guid Id { get; set; }
+    /// <summary>
+    /// Not used yet
+    /// </summary>
+    public Guid UserId { get; set; }
+    public long Price { get; set; }
+    public TypeEntertainment TypeEntertainment { get; set; }
+    public string? Details { get; set; }
+    public Area Area { get; set; }
+    /// <summary>
+    /// Maybe not worked with double
+    /// </summary>
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
