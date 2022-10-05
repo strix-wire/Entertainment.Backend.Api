353ff2ab63234de1e7f0714ea12da37bd2c06857
Vasily Pavlov <Pavlov.V@nppstels.ru>
2022-10-05 14:36:16 +0700

    Query details entertainment

---
+++ Entertainment.Application/Entertainment/Queries/GetEntertainmentDetails/EntertainmentDetailsQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entertainment.Application.Entertainment.Queries.GetEntertainmentDetails;
+
+internal class EntertainmentDetailsQuery : IRequest<EntertainmentDetailsVm>
+{
+    public Guid Id { get; set; }
+    /// <summary>
+    /// Not used yet
+    /// </summary>
+    public Guid UserId { get; set; }
+}
