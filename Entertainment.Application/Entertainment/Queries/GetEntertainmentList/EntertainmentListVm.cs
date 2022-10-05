7be78201a409ed20a7faf5371272eaea17a3c14d
Vasily Pavlov <Pavlov.V@nppstels.ru>
2022-10-05 14:36:56 +0700

    Get list entertainment implementation

---
+++ Entertainment.Application/Entertainment/Queries/GetEntertainmentList/EntertainmentListVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entertainment.Application.Entertainment.Queries.GetEntertainmentList
+{
+    public class EntertainmentListVm
+    {
+        public IList<EntertainmentLookupDto> GetEntertainments { get; set; }
+    }
+}
