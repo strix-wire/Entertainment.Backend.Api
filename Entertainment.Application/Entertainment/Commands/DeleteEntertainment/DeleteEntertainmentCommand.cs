d48c2d9de1e7c266e953a4a562e86288b31555e1
Vasily Pavlov <Pavlov.V@nppstels.ru>
2022-10-05 14:35:21 +0700

    Commands delete implementation

---
+++ Entertainment.Application/Entertainment/Commands/DeleteEntertainment/DeleteEntertainmentCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entertainment.Application.Entertainment.Commands.DeleteEntertainment;
+
+internal class DeleteEntertainmentCommand : IRequest
+{
+    public Guid Id { get; set; }
+    /// <summary>
+    /// Not used yet
+    /// </summary>
+    public Guid UserId { get; set; }
+}
---
+++ Entertainment.Application/Entertainment/Commands/DeleteEntertainment/DeleteEntertainmentCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Entertainment.Application.Interfaces;
+using Entertainment.Domain;
+using MediatR;
+
+namespace Entertainment.Application.Entertainment.Commands.DeleteEntertainment;
+
+//DeleteEntertainmentCommand - query, Guid - response
+internal class DeleteEntertainmentCommandHandler : IRequestHandler<DeleteEntertainmentCommand>
+{
+    private readonly IEntertainmentDbContext _dbContext;
+
+    public DeleteEntertainmentCommandHandler(IEntertainmentDbContext dbContext) =>
+        _dbContext = dbContext;
+
+    public async Task<Unit> Handle(DeleteEntertainmentCommand request,
+        CancellationToken cancellationToken)
+    {
+        var entity = await _dbContext.Entertainments
+            .FindAsync(new object[] { request.Id }, cancellationToken);
+
+        if (entity == null)
+        {
+            //CHange EntryPointNotFoundException
+            throw new EntryPointNotFoundException(nameof(EntertainmentEntity));
+        }
+
+        _dbContext.Entertainments.Remove(entity);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
