e9aca515e224196375defedd9763703f7a4df6c5
Vasily Pavlov <Pavlov.V@nppstels.ru>
2022-10-05 14:34:44 +0700

    Commands CreateEntertainment implementation

---
+++ Entertainment.Application/Entertainment/Commands/CreateEntertainment/CreateEntertainmentCommand.cs
@@ -0,0 +1,21 @@
+﻿using Entertainment.Domain;
+using MediatR;
+
+namespace Entertainment.Application.Entertainment.Commands.CreateEntertainment;
+
+internal class CreateEntertainmentCommand : IRequest<Guid>
+{
+    /// <summary>
+    /// Not used yet
+    /// </summary>
+    public Guid UserId { get; set; }
+    public long Price { get; set; }
+    public TypeEntertainment TypeEntertainment { get; set; }
+    public string? Details { get; set; }
+    public Area Area { get; set; }
+    /// <summary>
+    /// Maybe not worked with double
+    /// </summary>
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
---
+++ Entertainment.Application/Entertainment/Commands/CreateEntertainment/CreateEntertainmentCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using Entertainment.Application.Interfaces;
+using Entertainment.Domain;
+using MediatR;
+
+namespace Entertainment.Application.Entertainment.Commands.CreateEntertainment;
+
+internal class CreateEntertainmentCommandHandler : IRequestHandler<CreateEntertainmentCommand, Guid>
+{
+    private readonly IEntertainmentDbContext _dbContext;
+
+    public CreateEntertainmentCommandHandler(IEntertainmentDbContext dbContext) =>
+        _dbContext = dbContext;
+    
+    public async Task<Guid> Handle(CreateEntertainmentCommand request,
+        CancellationToken cancellationToken)
+    {
+        var entertainment = new EntertainmentEntity
+        {
+            Id = Guid.NewGuid(),
+            UserId = request.UserId,
+            Price = request.Price,
+            TypeEntertainment = request.TypeEntertainment,
+            Details = request.Details,
+            Area = request.Area,
+            Latitude = request.Latitude,
+            Longitude = request.Longitude,
+            CreationDate = DateTime.Now,
+            EditDate = null,
+        };
+
+        await _dbContext.Entertainments.AddAsync(entertainment, cancellationToken);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return entertainment.Id;
+    }
+
+}
