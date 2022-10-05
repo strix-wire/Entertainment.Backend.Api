6fce7b1ddf90f450b87ba29b2d17896e9abf77f4
Vasily Pavlov <Pavlov.V@nppstels.ru>
2022-10-05 14:47:54 +0700

    Create EntertainmentController

---
+++ Entertainment.Backend.Api/Controllers/EntertainmentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Entertainment.Backend.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[Controller]")]
+    public class EntertainmentController : BaseController
+    {
+        private readonly ILogger<EntertainmentController> _logger;
+
+        public EntertainmentController(ILogger<EntertainmentController> logger) =>
+            (_logger) = logger;
+
+    }
+}
